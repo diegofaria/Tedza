75a3f9bba537307e52c8466374d0ca4c6a1658a0
Rafael Fedelix <rafaelfedelix@gmail.com>
2012-05-29 22:36:35 -0300

    Correcao problema ao carregar video

--- web-app/js/script.js
+++ web-app/js/script.js
@@ -25,7 +25,7 @@ $(function() {
 	        function(data) {
 			  	var videoDiv = $("#video-player");
 			  	if (data.length > 0) {	
-			  		player.src(data[0][0].high);
+			  		player.src(data[0].high);
 			  		$("#q3").fadeOut("fast", function() {
 			  			$("#q3").after(videoDiv);
 			  			videoDiv.fadeIn("slow");
