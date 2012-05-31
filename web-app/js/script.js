dbb117f424b160a31942810391dd13843c996d7c
Rafael Fedelix <rafaelfedelix@gmail.com>
2012-05-30 21:25:01 -0300

    Remocao do footer e player auto start

--- web-app/js/script.js
+++ web-app/js/script.js
@@ -27,8 +27,8 @@ $(function() {
 			  	if (data.length > 0) {	
 			  		player.src(data[0].high);
 			  		$("#q3").fadeOut("fast", function() {
-			  			$("#q3").after(videoDiv);
 			  			videoDiv.fadeIn("slow");
+			  			player.play();
 			  		});
 			  	}
 			  	else {
