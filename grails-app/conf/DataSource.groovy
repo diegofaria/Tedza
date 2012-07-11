985f42e509f9e93d3346fce79f09ff50134485b5
Daniel Monti S. Silva <daniel.monti@informant.com.br>
2012-07-11 17:50:50 -0300

    Heroku tests.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,45 +1,70 @@
-dataSource {
-    dialect = net.sf.hibernate.dialect.PostgreSQLDialect
-    driverClassName = "org.postgresql.Driver"
-    pooled = true
-    username = "sa"
-    password = ""
-}
-hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = true
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+dataSource
+{ pooled = true }
+hibernate
+{
+	cache.use_query_cache = true
+	cache.use_second_level_cache = true
+	cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
 // environment specific settings
-environments {
-    development {
-        dataSource {
-            url = "jdbc:postgresql://localhost:5432/tedza"
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            driverClassName = "org.postgresql.Driver"
-            username = "tedza"
-            password = "123456"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
-        }
-    } 
-    production {
-        dataSource {
-            dataSource {
-                dbCreate = "create-drop"
-                driverClassName = "org.postgresql.Driver"
-                dialect = org.hibernate.dialect.PostgreSQLDialect
-            
-                uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+environments
+{
+	development
+	{
+		dataSource
+		{
+			dbCreate = "create-drop" // create-drop, update
+			driverClassName = "org.postgresql.Driver"
+			dialect = org.hibernate.dialect.PostgreSQLDialect
+			url = "jdbc:postgresql://localhost:5432/tedza"
+			username = "tedza"
+			password = "123456"
+			properties
+			{
+				maxActive = -1
+				minEvictableIdleTimeMillis=1800000
+				timeBetweenEvictionRunsMillis=1800000
+				numTestsPerEvictionRun=3
+				testOnBorrow=true
+				testWhileIdle=true
+				testOnReturn=true
+				validationQuery="SELECT 1"
+			}
+		}
+	}
+	test
+	{
+		dataSource
+		{
+			dbCreate = "update";
+			driverClassName = "org.h2.Driver";
+			url = "jdbc:h2:mem:storeTestDb;MVCC=TRUE";
+		}
+	}
+	production
+	{
+		dataSource
+		{
+			dbCreate = "update"
+			driverClassName = "org.postgresql.Driver"
+			dialect = org.hibernate.dialect.PostgreSQLDialect
 
-                url = "jdbc:postgresql://"+uri.host+uri.path
-                username = uri.userInfo.split(":")[0]
-                password = uri.userInfo.split(":")[1]
-            }
-        }
-    }
+			uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+
+			url = "jdbc:postgresql://"+uri.host+uri.path
+			username = uri.userInfo.split(":")[0]
+			password = uri.userInfo.split(":")[1]
+			properties
+			{
+				maxActive = -1
+				minEvictableIdleTimeMillis=1800000
+				timeBetweenEvictionRunsMillis=1800000
+				numTestsPerEvictionRun=3
+				testOnBorrow=true
+				testWhileIdle=true
+				testOnReturn=true
+				validationQuery="SELECT 1"
+			}
+		}
+	}
 }
